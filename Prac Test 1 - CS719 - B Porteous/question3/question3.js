22336c1a0efb8db51ad13dd614e08b0a01ee1384
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-08-27 09:43:13 +1200

    Beginning setting const JS variables

--- Prac Test 1 - CS719 - B Porteous/question3/question3.js
+++ Prac Test 1 - CS719 - B Porteous/question3/question3.js
@@ -1,3 +1,29 @@
-Window.addEventListener(“load”, function()) { 
-      
+window.addEventListener(“load”, function()) { 
+    
+    const newYork = "#ny";
+    const paris = "#paris";
+    const tokyo = "#tokyo";
+    const sydney = "#sydney";
+    const rdj = "#rdj";
+    const ct = "#ct";
+    const dubai = "#dubai";
+    
+
+	}) 
+
+
 }
+
+/*
+ny = new york
+paris
+tokyo
+sydney
+rdj = Rio de Janiero
+ct = cape town
+dubai
+ba = Buenos Aires
+london
+hk = Hong Kong
+rome
+sf = San Francisco
