00f545ac620f173b16c22dc9b967c3ddab5fc36d
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-09-03 12:56:35 +1200

    Completed Lab4bE2

--- Lab 04b - Arrays-Conditionals-Loops/exercises/ex02/to-do-list.js
+++ Lab 04b - Arrays-Conditionals-Loops/exercises/ex02/to-do-list.js
@@ -1,14 +1,20 @@
 window.addEventListener("load", function () {
 
     // TODO part 1: Create your to-do list here with some initial items.
-    const toDoList = null;
+    const toDoList = ["Eat Breakfast", "Eat Lunch", "Eat Dinner", "Eat Dessert", "Eat Midnight Snack"];
 
     // Get page elements where we write out the result
     const span = document.querySelector("#count");
     const ol = document.querySelector("#list");
 
     // TODO part 2:  Dispay the length of the toDoList in the span.
+    span.innerText = toDoList.length;
 
     // TODO part 3:  Iterate through the toDoList and add each as a list item.
+    toDoList.forEach (function(listItem) {
+       console.log(listItem);
+       ol.innerHTML += `<li type="a">${listItem}</li>`;
+    })
+
 
 });
