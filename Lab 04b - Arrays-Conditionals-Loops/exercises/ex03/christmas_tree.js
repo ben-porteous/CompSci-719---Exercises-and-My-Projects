243d8e3aa3cf978e3b758ebbe506a7f7766ef141
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-09-03 13:12:10 +1200

    Completed Lab4bE3

--- Lab 04b - Arrays-Conditionals-Loops/exercises/ex03/christmas_tree.js
+++ Lab 04b - Arrays-Conditionals-Loops/exercises/ex03/christmas_tree.js
@@ -1,5 +1,16 @@
 window.addEventListener("load", function () {
 
     // TODO Your code here.
+    const bauble = document.querySelectorAll(".bauble");
+
+    console.log(bauble);
+
+    bauble.forEach (function(ball) {
+        ball.addEventListener("click", function() {
+            console.log(event.target);
+            ball.classList.toggle("animated");
+
+        })
+    })
 
 });
