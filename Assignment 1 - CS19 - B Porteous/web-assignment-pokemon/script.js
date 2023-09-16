11f1df247350322a0752487fabf7849f04de9bf9
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-09-16 17:40:10 +1200

    added specific pokemon data into main content (1/2) - no random

--- Assignment 1 - CS19 - B Porteous/web-assignment-pokemon/script.js
+++ Assignment 1 - CS19 - B Porteous/web-assignment-pokemon/script.js
@@ -1,4 +1,50 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
+    //Variables from HTML:
+    const lsidebar = document.querySelector("#lsidebar");
+    const pokemonDataHeading = document.querySelector("#pokemon-data");
+    const pokemonDetailsHeading = document.querySelector("#pokemon-details")
+    const content = document.querySelector("#content")
+    const pokemonImage = document.querySelector("#pokemon-image")
+    const pokemonNumber = document.querySelector("#pokemon-number")
+    const pokemonDescription = document.querySelector("#pokemon-description")
+
+    //function to fetch of list of all pokemon and list on website
+    async function getAllPokemon() {
+        const allPokemonString = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon`);
+        const allPokemonJson = await allPokemonString.json();
+        allPokemonJson.forEach(function (pokemon) {
+            const pokemonName = pokemon.name;
+            lsidebar.innerHTML += `<p>${pokemonName}</p>`
+        })
+    }
+
+    //function to get random pokemon's data
+    async function getRandomPokemon() {
+        const randomPokemonString = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/random`);
+        const randomPokemonJson = await randomPokemonString.json();
+        return randomPokemonJson;
+    }
+
+    //function to load show indiv pokemon's data into the site:
+    async function loadPokemonData (pokemon) {
+        const specificPokemonString = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/${pokemon}`);
+        const specificPokemonJson = await specificPokemonString.json();
+
+        pokemonDataHeading.innerText += ` ${specificPokemonJson.name}`;
+        pokemonDetailsHeading.innerText += ` ${specificPokemonJson.name}`;
+        pokemonNumber.innerText += ` ${specificPokemonJson.dexNumber}`;
+        pokemonDescription.innerText += ` ${specificPokemonJson.dexEntry}`;
+        pokemonImage.setAttribute("src", `${specificPokemonJson.imageUrl}`);
+    }
+
+
+
+    getAllPokemon();
+
+    // getRandomPokemon();
+    loadPokemonData(60);
 
 });
+
+//
