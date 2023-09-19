86c3fc9b0655a33838dded6cafa0c4dd880914c4
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-09-19 19:28:52 +1200

    Added Event Listener

--- Assignment 1 - CS19 - B Porteous/web-assignment-pokemon/script.js
+++ Assignment 1 - CS19 - B Porteous/web-assignment-pokemon/script.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", async function () {
 
     //Variables from HTML:
     const lsidebar = document.querySelector("#lsidebar");
@@ -8,6 +8,8 @@ window.addEventListener("load", function () {
     const pokemonImage = document.querySelector("#pokemon-image")
     const pokemonNumber = document.querySelector("#pokemon-number")
     const pokemonDescription = document.querySelector("#pokemon-description")
+    const allPokemonTitle = document.querySelector("#all-pokemon-title")
+    let i = 1
 
 
     //function to fetch of list of all pokemon and list on website
@@ -16,23 +18,32 @@ window.addEventListener("load", function () {
         const allPokemonJson = await allPokemonString.json();
         allPokemonJson.forEach(function (pokemon) {
             const pokemonName = pokemon.name;
-            lsidebar.innerHTML += `<p>${pokemonName}</p>`
-        })
+            const newPokemonPara = document.createElement("p")
+            newPokemonPara.innerText = `${pokemonName}`
+            newPokemonPara.setAttribute("id", `id-${i}`)
+            newPokemonPara.classList.add("pokemonList");
+            allPokemonTitle.appendChild(newPokemonPara);
+            newPokemonPara.addEventListener("click", async function(){
+                await loadPokemonData(`${pokemon.dexNumber}`)
+            })
+            i++ ;
+        }) 
     }
+
 
     //function to load & show indiv pokemon's data into the site:
     async function loadPokemonData (pokemon) {
         const specificPokemonString = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/${pokemon}`);
         const specificPokemonJson = await specificPokemonString.json();
 
-        pokemonDataHeading.innerText += ` ${specificPokemonJson.name}`;
-        pokemonDetailsHeading.innerText += ` ${specificPokemonJson.name}`;
-        pokemonNumber.innerText += ` ${specificPokemonJson.dexNumber}`;
-        pokemonDescription.innerText += ` ${specificPokemonJson.dexEntry}`;
+        pokemonDataHeading.innerText = `Pokemon Type Data for ${specificPokemonJson.name}`;
+        pokemonDetailsHeading.innerText = `Pokemon Details for ${specificPokemonJson.name}`;
+        pokemonNumber.innerText = `Pokedex Number: ${specificPokemonJson.dexNumber}`;
+        pokemonDescription.innerText = ` ${specificPokemonJson.dexEntry}`;
         pokemonImage.setAttribute("src", `${specificPokemonJson.imageUrl}`);
     }
 
-    //function to get random pokemon's data - is this needed, or just load once page loaded?????
+    //function to get and load random pokemon's data
     async function getRandomPokemon() {
         const randomPokemonString = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/random`);
         const randomPokemonJson = await randomPokemonString.json();
@@ -40,13 +51,16 @@ window.addEventListener("load", function () {
         loadPokemonData(randomDex);
     }
 
+    // //Event click handler so if someone clicks on a pokemon on the side the page loads that pokemon's data:
+    // async function addEventListener(pokemon) {
+    //     target.addEventListener(event.target)
 
+    // }
 
-
-
-    getAllPokemon();
-    getRandomPokemon();
+   await getAllPokemon();
+   await getRandomPokemon();
     // loadPokemonData(123);
+
 
 });
 
