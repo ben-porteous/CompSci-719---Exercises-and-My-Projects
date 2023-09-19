73550ee5b439fd140a03e07b242b316621f038c7
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-09-19 21:35:22 +1200

    further tidying A1

--- Assignment 1 - CS19 - B Porteous/web-assignment-pokemon/script.js
+++ Assignment 1 - CS19 - B Porteous/web-assignment-pokemon/script.js
@@ -3,13 +3,13 @@ window.addEventListener("load", async function () {
     //Variables from HTML:
     const lsidebar = document.querySelector("#lsidebar");
     const pokemonDataHeading = document.querySelector("#pokemon-data");
+    const pokemonDataTypes = document.querySelector("#pokemon-data-types")
     const pokemonDetailsHeading = document.querySelector("#pokemon-details")
     const content = document.querySelector("#content")
     const pokemonImage = document.querySelector("#pokemon-image")
     const pokemonNumber = document.querySelector("#pokemon-number")
     const pokemonDescription = document.querySelector("#pokemon-description")
-    const allPokemonTitle = document.querySelector("#all-pokemon-title")
-    let i = 1
+    // const allPokemonTitle = document.querySelector("#all-pokemon-title")
 
 
     // //function to fetch of list of all pokemon and list on website
@@ -41,30 +41,34 @@ window.addEventListener("load", async function () {
     function loadAllPokemon(pokemon) {
         const pokemonName = pokemon.name;
         const newPokemonPara = document.createElement("p")
-        newPokemonPara.innerText = `${pokemonName}`
-        newPokemonPara.setAttribute("id", `id-${i}`)
-        newPokemonPara.classList.add("pokemonList");
-        allPokemonTitle.appendChild(newPokemonPara);
-        newPokemonPara.addEventListener("click", async function(){
-            await loadPokemonData(`${pokemon.dexNumber}`)
+        newPokemonPara.innerText = pokemonName
+        // newPokemonPara.setAttribute("id", `id-${pokemon.dexNumber}`) // potentially remove, unless needed?
+        // newPokemonPara.classList.add("pokemonList"); // potentially remove, unless needed?
+        lsidebar.appendChild(newPokemonPara);
+        newPokemonPara.addEventListener("click", async function () {
+            await loadPokemonData(pokemon.dexNumber)
         })
-        i++ ;
     }
+
+
 
 
 
 
     //function to load & show an individ's pokemon's data into the site:
-    async function loadPokemonData (pokemon) {
+    async function loadPokemonData(pokemon) {
         const specificPokemonString = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/${pokemon}`);
         const specificPokemonJson = await specificPokemonString.json();
-
-        pokemonDataHeading.innerText = `Pokemon Type Data for ${specificPokemonJson.name}`;
+        pokemonDataHeading.innerHTML = `Pokemon Type Data for ${specificPokemonJson.name}`;
         pokemonDetailsHeading.innerText = `Pokemon Details for ${specificPokemonJson.name}`;
         pokemonNumber.innerText = `Pokedex Number: ${specificPokemonJson.dexNumber}`;
-        pokemonDescription.innerText = ` ${specificPokemonJson.dexEntry}`;
-        pokemonImage.setAttribute("src", `${specificPokemonJson.imageUrl}`);
+        pokemonDescription.innerText = specificPokemonJson.dexEntry;
+        pokemonImage.setAttribute("src", specificPokemonJson.imageUrl);
+        pokemonDataTypes.innerHTML = specificPokemonJson.types.reduce(function (acc, current) {
+            return acc + ", " + current;
+        });
     }
+
 
     //function to get and load random pokemon's data
     async function getRandomPokemon() {
@@ -74,16 +78,17 @@ window.addEventListener("load", async function () {
         loadPokemonData(randomDex);
     }
 
-    // //Event click handler so if someone clicks on a pokemon on the side the page loads that pokemon's data:
-    // async function addEventListener(pokemon) {
-    //     target.addEventListener(event.target)
 
-    // }
+    await getAllPokemon();
+    await getRandomPokemon();
 
-   await getAllPokemon();
-   await getRandomPokemon();
-    // loadPokemonData(123);
 
+
+
+
+    //Get Type Data for a pokemon and load it
+    //    https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/${i.types}
+    //    https://cs719-a01-pt-server.trex-sandwich.com/api/types/:typeName
 
 });
 
