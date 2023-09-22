164c1b354f796248019046c709c8c007dfec0d35
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-09-22 19:01:46 +1200

    Created Table function

--- Assignment 1 - CS19 - B Porteous/web-assignment-pokemon/script.js
+++ Assignment 1 - CS19 - B Porteous/web-assignment-pokemon/script.js
@@ -10,25 +10,9 @@ window.addEventListener("load", async function () {
     const pokemonNumber = document.querySelector("#pokemon-number")
     const pokemonDescription = document.querySelector("#pokemon-description")
     // const allPokemonTitle = document.querySelector("#all-pokemon-title")
+    const offence = document.querySelector("#offence")
+    const defence = document.querySelector("#defence")
 
-
-    // //function to fetch of list of all pokemon and list on website
-    // async function getAllPokemon() {
-    //     const allPokemonString = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon`);
-    //     const allPokemonJson = await allPokemonString.json();
-    //     allPokemonJson.forEach(function (pokemon) {
-    //         const pokemonName = pokemon.name;
-    //         const newPokemonPara = document.createElement("p")
-    //         newPokemonPara.innerText = `${pokemonName}`
-    //         newPokemonPara.setAttribute("id", `id-${i}`)
-    //         newPokemonPara.classList.add("pokemonList");
-    //         allPokemonTitle.appendChild(newPokemonPara);
-    //         newPokemonPara.addEventListener("click", async function(){
-    //             await loadPokemonData(`${pokemon.dexNumber}`)
-    //         })
-    //         i++ ;
-    //     }) 
-    // }
 
 
     //function to fetch of list of all pokemon and list on website
@@ -37,23 +21,23 @@ window.addEventListener("load", async function () {
         const allPokemonJson = await allPokemonString.json();
         allPokemonJson.forEach(loadAllPokemon)
     }
-
     function loadAllPokemon(pokemon) {
         const pokemonName = pokemon.name;
         const newPokemonPara = document.createElement("p")
         newPokemonPara.innerText = pokemonName
-        // newPokemonPara.setAttribute("id", `id-${pokemon.dexNumber}`) // potentially remove, unless needed?
-        // newPokemonPara.classList.add("pokemonList"); // potentially remove, unless needed?
         lsidebar.appendChild(newPokemonPara);
         newPokemonPara.addEventListener("click", async function () {
             await loadPokemonData(pokemon.dexNumber)
         })
     }
 
-
-
-
-
+    //function to get and load random pokemon's data 
+    async function getRandomPokemon() {
+        const randomPokemonString = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/random`);
+        const randomPokemonJson = await randomPokemonString.json();
+        const randomDex = await randomPokemonJson.dexNumber;
+        loadPokemonData(randomDex);
+    }
 
     //function to load & show an individ's pokemon's data into the site:
     async function loadPokemonData(pokemon) {
@@ -67,16 +51,11 @@ window.addEventListener("load", async function () {
         pokemonDataTypes.innerHTML = specificPokemonJson.types.reduce(function (acc, current) {
             return acc + ", " + current;
         });
-    }
+        specificPokemonJson.types.forEach(pokemonTypeFunction)
+    };
 
 
-    //function to get and load random pokemon's data
-    async function getRandomPokemon() {
-        const randomPokemonString = await fetch(`https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/random`);
-        const randomPokemonJson = await randomPokemonString.json();
-        const randomDex = await randomPokemonJson.dexNumber;
-        loadPokemonData(randomDex);
-    }
+
 
 
     await getAllPokemon();
@@ -86,10 +65,70 @@ window.addEventListener("load", async function () {
 
 
 
+
+    //function to make a table
+    function tableCreator(subheading1, subheading2) {
+        const table = document.createElement("table")
+        const tableRow = document.createElement("tr")
+        const tableCell1 = document.createElement("td")
+        const tableCell2 = document.createElement("td")
+        const tableCell3 = document.createElement("td")
+        const tableCell4 = document.createElement("td")
+
+        table.appendChild(tableRow)
+        tableRow.appendChild(tableCell1)
+        tableRow.appendChild(tableCell2)
+        tableCell1.innerHTML = subheading1
+        tableCell2.innerHTML = subheading2
+
+    }
+
+
+
     //Get Type Data for a pokemon and load it
-    //    https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/${i.types}
-    //    https://cs719-a01-pt-server.trex-sandwich.com/api/types/:typeName
+    //    https://cs719-a01-pt-server.trex-sandwich.com/api/pokemon/${type}
+    //    https://cs719-a01-pt-server.trex-sandwich.com/api/types/${type}
 
-});
+    //https://cs719-a01-pt-server.trex-sandwich.com/api/types/grass
 
-//
+// let t = 0
+    async function pokemonTypeFunction(type) {
+        offence.innerHTML += tableCreator(`${type} attacks`, `${type} attackers`)
+        
+
+    }
+        //The headings need to be: "Defending type" "Damage Dealt"
+
+//         pokeTypeDataString = await fetch(`https:cs719-a01-pt-server.trex-sandwich.com/api/types/${type}`) // website for individual types. in site is .offenceM[type].
+//         pokeTypeDataJson = await pokeTypeDataString.json()
+//         console.log(pokeTypeDataJson)
+//         tableData.innerHTML = `${type} type attacks:`
+//         //Works up to here//
+
+
+
+        // tableData.innerHTML = `
+        // <td> ${pokeTypeDataJson}.offenceDamageMultipliers[${t}].type </td>
+        // <td> ${pokeTypeDataJson}.offenceDamageMultipliers[${t}].multiplier </td>
+        // `
+
+    // };
+// });
+
+//add ; to the end of each line
+
+// const li = fragment
+//   .appendChild(document.createElement("section"))
+//   .appendChild(document.createElement("ul"))
+//   .appendChild(document.createElement("li"));
+// li.textContent = "hello world";
+
+
+// const offenceTable = fragment
+// .appendChild(document.createElement("table"))
+// .appendChild(document.createElement("tr"))
+// .appendChild(document.createElement("td"))
+// td.textContent
+
+
+})
