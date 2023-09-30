a5cb3fb9b2f52476c62e1bf491ac99825cec6f04
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-09-30 14:41:07 +1300

    L7E5 All done except dual post-get functionality

--- Lab7 - NodeJS and Express/exercise-05/exercise-05.js
+++ Lab7 - NodeJS and Express/exercise-05/exercise-05.js
@@ -11,6 +11,26 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
 // TODO Your code here.
+app.post("/form", function (req, res) {
+    console.log("server responding")
+    const userData = {
+        name: req.body.name,
+        email: req.body.email,
+        city: req.body.city
+    }
+
+    const userResponses = {
+        courseThoughts: req.body.thoughts,
+        courseExperience: req.body.experience,
+        favAnimal: req.body.mammal,
+        additionalComments: req.body.comments
+    }
+
+
+    res.json([userData, userResponses])
+
+})
+
 
 
 // Start the server running
