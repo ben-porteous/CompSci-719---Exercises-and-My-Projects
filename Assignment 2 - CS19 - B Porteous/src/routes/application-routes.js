258c0fa9710492c4f89ed28c070f1ec8c155d105
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-10-04 08:03:30 +1300

    A2Q2 done except ids & classes

--- Assignment 2 - CS19 - B Porteous/src/routes/application-routes.js
+++ Assignment 2 - CS19 - B Porteous/src/routes/application-routes.js
@@ -4,10 +4,15 @@ const router = express.Router();
 
 router.get("/", function (req, res) {
   // TODO Add necessary data to res.locals before rendering the "home" page.
-  const pokemon = getAllPokemon()
+  const pokemon = getAllPokemon();
 
-  res.locals.pokemon = pokemon
-  res.locals.openingPokemonImage = pokemon[57].imageUrl
+  res.locals.pokemon = pokemon;
+  res.locals.openingPokemonImage = pokemon[57].imageUrl;
+  res.locals.openingPokemonNumber = pokemon[57].dexNumber;
+  res.locals.openingPokemonName = pokemon[57].name;
+  res.locals.openingPokemonTypes = pokemon[57].types;
+  res.locals.openingPokemonAbout = pokemon[57].dexEntry;
+
   res.render("home");
 });
 
