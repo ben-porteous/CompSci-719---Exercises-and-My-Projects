eaffab804d3f780a44300a7a6232493776e7fe33
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-10-07 15:56:26 +1300

    A2 T3 - json data provided when get request submitted with Dex Number

--- Assignment 2 - CS19 - B Porteous/src/routes/api/pokemon-api-routes.js
+++ Assignment 2 - CS19 - B Porteous/src/routes/api/pokemon-api-routes.js
@@ -5,5 +5,17 @@ const router = express.Router();
 // When sending a GET request for /:dexNumber (:dexNumber is a path param),
 // return the JSON representation of the correct Pokemon, or return a 404 error
 // if that Pokemon is not found.
+router.get("/test", function (req, res) {
+    res.send("TESTING")
+})
+
+router.get("/:dexNumber", function (req, res) {
+
+    const pokemonJson = require("../../db/pokemon-db.js")
+    console.log(pokemonJson.getPokemonByDexNumber(req.params.dexNumber))
+
+    res.json(pokemonJson.getPokemonByDexNumber(req.params.dexNumber))
+});
+
 
 module.exports = router;
