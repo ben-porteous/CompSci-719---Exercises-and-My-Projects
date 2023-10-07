42b6b0ba28592a34f2b287301445e599aa5e7e73
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-10-07 13:57:03 +1300

    PT2 Q2 - adding Home coloring when on home page

--- Prac Test 2 - CS719 - B Porteous/question-02/routes/question-two-routes.js
+++ Prac Test 2 - CS719 - B Porteous/question-02/routes/question-two-routes.js
@@ -7,14 +7,18 @@ const router = express.Router();
 
 // TODO Your Code Here
 router.get("/", function (req, res) {
+    res.locals.name = "Home"
+    res.locals.homePage = "home"
     res.render("home")
 })
 
 router.get("/hillary", function (req, res) {
+    res.locals.name = "Edmund Hillary"
     res.render("hillary")
 })
 
 router.get("/sheppard", function (req, res) {
+    res.locals.name = "Kate Sheppard"
     res.render("sheppard")
 })
 
